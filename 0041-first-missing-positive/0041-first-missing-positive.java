99a04645987203d243bece5f2bc593ec5cc26640
RITHIGA M K <rithigamanjunathan@gmail.com>
2026-09-15 10:06:43 +0530

    Time: 20 ms (7.22%), Space: 90.8 MB (11.43%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -5,14 +5,11 @@ class Solution {
      int miss = 1;
      for(int i=0 ; i<nums.length;i++)
      {
-       set.add(nums[i]);
-     }
-      for(int i=0 ; i<nums.length;i++)
-      {
-        while(set.contains(miss))
-        {
+    set.add(nums[i]);
+    while(set.contains(miss))
+    {
             miss++;
-        }
+     }
       }
     return miss;   
     }   
