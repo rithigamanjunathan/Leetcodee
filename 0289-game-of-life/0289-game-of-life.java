85a15c5327465bb57f7738953543ebea03e40258
RITHIGA M K <rithigamanjunathan@gmail.com>
2026-09-16 12:09:39 +0530

    Time: 0 ms (100%), Space: 43.2 MB (46.68%) - LeetHub

---
+++ 0289-game-of-life/0289-game-of-life.java
@@ -0,0 +1,49 @@
+class Solution {
+    public void gameOfLife(int[][] board) {
+
+        int m = board.length;
+        int n = board[0].length;
+
+        int[][] a = new int[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                a[i][j] = board[i][j];
+            }
+        }
+
+        for (int r = 0; r < m; r++) {
+            for (int c = 0; c < n; c++) {
+
+                int count = 0;
+
+    
+                for (int i = r - 1; i <= r + 1; i++) {
+                    for (int j = c - 1; j <= c + 1; j++) {
+                        if (i < 0 || i >= m || j < 0 || j >= n) {
+                            continue;
+                        }
+                        if (i == r && j == c) {
+                            continue;
+                        }
+
+                        if (a[i][j] == 1) {
+                            count++;
+                        }
+                    }
+                }
+
+              
+                if (a[r][c] == 1) {
+                    if (count < 2 || count > 3) {
+                        board[r][c] = 0;
+                    }
+                }
+                else {
+                    if (count == 3) {
+                        board[r][c] = 1;
+                    }
+                }
+            }
+        }
+    }}
