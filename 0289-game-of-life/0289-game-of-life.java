b33d6918ca41fddc8760c832d2fdf201d6e98482
RITHIGA M K <rithigamanjunathan@gmail.com>
2026-09-16 14:31:39 +0530

    Time: 0 ms (100%), Space: 43.3 MB (46.68%) - LeetHub

--- 0289-game-of-life/0289-game-of-life.java
+++ 0289-game-of-life/0289-game-of-life.java
@@ -19,21 +19,15 @@ class Solution {
 
     
                 for (int i = r - 1; i <= r + 1; i++) {
-                    for (int j = c - 1; j <= c + 1; j++) {
-                        if (i < 0 || i >= m || j < 0 || j >= n) {
-                            continue;
+                 for (int j = c - 1; j <= c + 1; j++) {
+                        if (i >= 0 && i < m && j >= 0 && j < n) {
+                        if (!(i == r && j == c)) {
+                            if (a[i][j] == 1)
+                            count+=a[i][j];
                         }
-                        if (i == r && j == c) {
-                            continue;
-                        }
-
-                        if (a[i][j] == 1) {
-                            count++;
-                        }
+                        } 
                     }
                 }
-
-              
                 if (a[r][c] == 1) {
                     if (count < 2 || count > 3) {
                         board[r][c] = 0;
