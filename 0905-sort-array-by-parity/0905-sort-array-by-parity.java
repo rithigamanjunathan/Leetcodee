a25c8d500825b858a74a36cd51e9f63bef4398ad
RITHIGA M K <rithigamanjunathan@gmail.com>
2026-09-17 12:11:14 +0530

    Time: 1 ms (44.12%), Space: 46.7 MB (69.46%) - LeetHub

---
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] sortArrayByParity(int[] nums)
+    {
+    int[] arr = new int[nums.length];
+    int k=0;
+      for(int i=0 ; i<nums.length ; i++)
+      {
+        if(nums[i]%2==0)
+        {
+            arr[k] = nums[i];
+            k++;
+        }
+      }
+      for(int i=0 ; i<nums.length ; i++)
+      {
+        if(nums[i]%2!=0)
+        {
+            arr[k] = nums[i];
+            k++;
+        }
+      } 
+      return arr;  
+    }
+}
