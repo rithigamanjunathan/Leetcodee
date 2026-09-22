5285c052dfda5e5d7e97903e2715b201d3a0f52f
RITHIGA M K <rithigamanjunathan@gmail.com>
2026-09-22 11:22:17 +0530

    Time: 0 ms (100%), Space: 48.2 MB (73.89%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void reverseString(char[] s) 
+    {
+       char temp;
+       int l=0,r=s.length-1;
+       while(l<r)
+       {
+        temp = s[l];
+        s[l] = s[r];
+        s[r] = temp;
+        l++;r--;
+       }
+    }
+}
